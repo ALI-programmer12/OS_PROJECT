351f31172ffdd5e167dbc98d2f2aea1a64b15968
Ali Ahmed Burney <you@example.com>
2026-04-20 23:31:40 +0500

    added comments in sjf

--- src/sjf.cpp
+++ src/sjf.cpp
@@ -1,28 +1,29 @@
 #include "../include/sjf.h"
 #include <iostream>
-#include <limits>
+#include <climits>
 using namespace std;
 
 void runSJF(vector<Process>& list) {
-    int n = static_cast<int>(list.size());
+    int n = list.size(); // number of processes
 
     for (auto &p : list)
-        p.completed = false;
+        p.completed = false; // Marks all processes as not completed
 
-    int completedCount = 0;
-    int time = 0;
+    int completedCount = 0; // Tracks how many processes are done
+    int time = 0; // simulation clock (CPU time starts at 0 which is in Ghant chart)
 
     while (completedCount < n) {
         int idx = -1;
-        int minBurst = numeric_limits<int>::max();
+        int minBurst = INT_MAX;
 
         for (int i = 0; i < n; i++) {
             if (!list[i].completed && list[i].arrivalTime <= time) {
                 if (list[i].burstTime < minBurst) {
-                    minBurst = list[i].burstTime;
+                    minBurst = list[i].burstTime; // store the smallest burst time process
                     idx = i;
-                } else if (list[i].burstTime == minBurst) {
-                    if (idx == -1 || list[i].arrivalTime < list[idx].arrivalTime)
+                } else if (list[i].burstTime == minBurst) // Tie b/w 2 processes then: 
+                { 
+                    if (idx == -1 || list[i].arrivalTime < list[idx].arrivalTime) //earlier arrival time wins
                         idx = i;
                 }
             }
