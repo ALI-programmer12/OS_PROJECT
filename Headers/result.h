d2696d511855ac519e57cca57e75a9d3c222928e
unknown <jumer2380@gmail.com>
2026-04-23 21:19:46 +0500

    Raylib input

--- Headers/result.h
+++ Headers/result.h
@@ -2,11 +2,16 @@
 #define RESULT_H
 
 #include <string>
+#include <vector>
+#include <utility>
+#include <tuple>
 
 struct Result {
     std::string name;
     float avgWT;
     float avgTAT;
+    std::vector<std::tuple<int, int, int>> gantt;
+    Result(std::string n, float wt, float tat, std::vector<std::tuple<int, int, int>> g) : name(n), avgWT(wt), avgTAT(tat), gantt(g) {}
 };
 
 #endif
